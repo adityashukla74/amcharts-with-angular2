7085c20c81d148e83c310aa953233c4b6245e0ed
Aditya Shukla <adityashukla@Adityas-MacBook-Pro.local>
2017-06-26 21:24:58 +0530

    charts for body added

--- src/client/app/about/about.component.ts
+++ src/client/app/about/about.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AmChartsService } from '@amcharts/amcharts3-angular';
-
+declare let chart:any
 @Component({
  moduleId: module.id,
  selector: 'sd-about',
@@ -8,85 +8,152 @@ import { AmChartsService } from '@amcharts/amcharts3-angular';
  styleUrls: ['about.component.css']
 })
 export class AboutComponent implements OnInit {
- private timer: any;
- private chart1: any;
-  private chart2: any;
+    private timer: any;
+    private chart1: any;
+    private chart2: any;
+    private chart3: any;
  constructor(private AmCharts: AmChartsService) { }
- makeRandomDataProvider() {
- var dataProvider = [];
- for (var year = 1950; year <= 2005; ++year) {
- dataProvider.push({
- year: '' + year,
- value: Math.floor(Math.random() * 100) - 50
- });
- }
- return dataProvider;
- }
- ngOnInit() {
- this.chart = this.AmCharts.makeChart(
- 'chart1div', {
- 'type': 'pie',
- "dataTableId": "chartdata",
- 'theme': 'light',
- 'dataProvider': [
- {
- 'title': 'New', 'value': 4852
- },
- {
- 'title': 'Returning',
- 'value': 9899
- }
- ],
- 'titleField': 'title',
- 'valueField': 'value',
- 'labelRadius': 5,
- 'radius': '42%',
- 'innerRadius': '60%',
- 'labelText': '[[title]]',
- }
- );
- this.chart = this.AmCharts.makeChart(
- 'chart2div', {
- 'type': 'pie',
- 'theme': 'light',
- 'dataProvider': [
- {
- 'title': 'New', 'value': 4852
- },
- {
- 'title': 'Returning',
- 'value': 9899
- }
- ],
- 'titleField': 'title',
- 'valueField': 'value',
- 'labelRadius': 5,
- 'radius': '42%',
- 'innerRadius': '60%',
- 'labelText': '[[title]]',
- }
- );
- this.chart = this.AmCharts.makeChart(
- 'chart3div', {
- 'type': 'pie',
- 'theme': 'light',
- 'dataProvider': [
- {
- 'title': 'New', 'value': 4852
- },
- {
- 'title': 'Returning',
- 'value': 9899
- }
- ],
- 'titleField': 'title',
- 'valueField': 'value',
- 'labelRadius': 5,
- 'radius': '42%',
- 'innerRadius': '60%',
- 'labelText': '[[title]]',
- }
- );
- }
+    makeRandomDataProvider() {
+        var dataProvider = [];
+        for (var year = 1950; year <= 2005; ++year) {
+            dataProvider.push({
+            year: '' + year,
+            value: Math.floor(Math.random() * 100) - 50
+            });
+        }  
+        return dataProvider;
+    }
+    ngOnInit() {
+        this.chart = this.AmCharts.makeChart(
+            'chart1div', {
+                'type': 'pie',
+                "dataTableId": "chartdata",
+                'theme': 'light',
+                'dataProvider': [
+                    {
+                    'title': 'New', 'value': 4852
+                    },
+                    {
+                    'title': 'Returning',
+                    'value': 9899
+                    }
+                ],
+                'titleField': 'title',
+                'valueField': 'value',
+                'labelRadius': 5,
+                'radius': '42%',
+                'innerRadius': '60%',
+                'labelText': '[[title]]',
+                }
+        );
+        this.chart = this.AmCharts.makeChart(
+            'chart2div', {
+                'type': 'pie',
+                'theme': 'light',
+                'dataProvider': [
+                    {
+                    'title': 'New', 'value': 4852
+                    },
+                    {
+                    'title': 'Returning',
+                    'value': 9899
+                    }
+                    ],
+                'titleField': 'title',
+                'valueField': 'value',
+                'labelRadius': 5,
+                'radius': '42%',
+                'innerRadius': '60%',
+                'labelText': '[[title]]',
+            }
+        );
+        this.chart = this.AmCharts.makeChart(
+            'chart3div', {
+                'type': 'pie',
+                'theme': 'light',
+                'dataProvider': [
+                    {
+                    'title': 'New', 'value': 4852
+                    },
+                    {
+                    'title': 'Returning',
+                    'value': 9899
+                    }
+                    ],
+                'titleField': 'title',
+                'valueField': 'value',
+                'labelRadius': 5,
+                'radius': '42%',
+                'innerRadius': '60%',
+                'labelText': '[[title]]',
+                }
+        );
+        this.chart = this.AmCharts.makeChart(
+            'chart4div', {
+                'type': 'pie',
+                "dataTableId": "chartdata",
+                'theme': 'light',
+                'dataProvider': [
+                    {
+                    'title': 'New', 'value': 4852
+                    },
+                    {
+                    'title': 'Returning',
+                    'value': 9899
+                    }
+                ],
+                'titleField': 'title',
+                'valueField': 'value',
+                'labelRadius': 5,
+                'radius': '42%',
+                'innerRadius': '60%',
+                'labelText': '[[title]]',
+                }
+        );
+        this.chart = this.AmCharts.makeChart(
+            'chart5div', {
+                'type': 'pie',
+                'theme': 'light',
+                'dataProvider': [
+                    {
+                    'title': 'New', 'value': 4852
+                    },
+                    {
+                    'title': 'Returning',
+                    'value': 9899
+                    }
+                    ],
+                'titleField': 'title',
+                'valueField': 'value',
+                'labelRadius': 5,
+                'radius': '42%',
+                'innerRadius': '60%',
+                'labelText': '[[title]]',
+            }
+        );
+        this.chart = this.AmCharts.makeChart(
+            'chart6div', {
+                'type': 'pie',
+                'theme': 'light',
+                'dataProvider': [
+                    {
+                    'title': 'New', 'value': 4852
+                    },
+                    {
+                    'title': 'Returning',
+                    'value': 9899
+                    }
+                    ],
+                'titleField': 'title',
+                'valueField': 'value',
+                'labelRadius': 5,
+                'radius': '42%',
+                'innerRadius': '60%',
+                'labelText': '[[title]]',
+                }
+        );
+    }
 }
 }
+
+
